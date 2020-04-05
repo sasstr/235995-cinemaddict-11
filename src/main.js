9260559c5c10a99de11be1c378ddaff716770aea
Alexander Strakhovenko <sasstr@gmail.com>
2020-04-06 01:00:26 +0300

    Добавил карточку фильма

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 'use strict';
+
+const CardsAmount = {
+  FILMS: 5,
+  TOP_RATED: 2,
+  MOST_COMMENTED: 2,
+};
+
 /**
  * Функция отрисовывает шаблон profile
  * @return {string} Шаблон profile
@@ -46,6 +53,39 @@ const createSortTemplate = () => {
     <li><a href="#" class="sort__button">Sort by rating</a></li>
   </ul>
   `.trim());
+};
+
+/**
+ * Функция отрисовывает шаблон меню сортировки
+ * @return {string} Шаблон меню сортировки
+ */
+const createFilmCardTemplate = () => {
+  return (`
+  <article class="film-card">
+          <h3 class="film-card__title">The Dance of Life</h3>
+          <p class="film-card__rating">8.3</p>
+          <p class="film-card__info">
+            <span class="film-card__year">1929</span>
+            <span class="film-card__duration">1h 55m</span>
+            <span class="film-card__genre">Musical</span>
+          </p>
+          <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
+          <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
+          <a class="film-card__comments">5 comments</a>
+          <form class="film-card__controls">
+            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+          </form>
+        </article>
+  `.trim());
+};
+
+const getFilmCards = (amount) => {
+
+  for (let i = 0; i < amount; i++) {
+    createFilmCardTemplate();
+  }
 };
 
 /**
