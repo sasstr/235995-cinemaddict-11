1001facd681e890a227aabad88c2eda071fc95ad
Alexander Strakhovenko <sasstr@gmail.com>
2020-04-07 08:24:38 +0300

    Добавляет коментарий к функции

--- src/main.js
+++ src/main.js
@@ -130,6 +130,10 @@ const createMostCommentedTemplate = () => {
   `.trim());
 };
 
+/**
+ * Функция отрисовывает шаблон кнопки
+ * @return {string} Шаблон кнопки
+ */
 const createShowMoreBtnTemplate = () => {
   return (`<button class="films-list__show-more">Show more</button>`.trim());
 };
